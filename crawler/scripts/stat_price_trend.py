b628d789d49f67f398eccf8b1ee1a7679d335ee0
hjy846 <work@iZj6cfu4aqltf16xu72qt0Z.(none)>
2017-02-21 11:15:08 +0800

    update price trend

--- crawler/scripts/stat_price_trend.py
+++ crawler/scripts/stat_price_trend.py
@@ -18,7 +18,7 @@ def get_param(sys):
     #处理时间
     if len(sys.argv) >= 2:
         crawl_date = sys.argv[1]
-    else: crawl_date = 'today'
+    else: crawl_date = 'yesterday'
 
     now = datetime.now()
     yesterday = now - timedelta(days=1)
@@ -30,83 +30,11 @@ def get_param(sys):
     elif crawl_date == 'yesterday':
         crawl_date = yesterday_str
     else:
-        crawl_date = crawl_date if crawl_date else today_str
+        crawl_date = crawl_date if crawl_date else yesterday_str
 
     #settings['crawl_date'] = self.crawl_date
     return {'crawl_date':crawl_date}
 
-def insert_into_all_residences():
-    server = settings['MONGODB_SERVER']
-    port = settings['MONGODB_PORT']
-    raw_db = settings['MONGODB_DB_RAW']
-    connection = pymongo.MongoClient(server, port)
-    raw_db = connection[raw_db]   
-    raw_collection = raw_db[PARAMS['crawl_date']]
-    res = raw_collection.find()
-    
-    image_collection = raw_db[settings['MONGODB_COLLECTION_IMAGE']]
-
-def find_new_residences():
-    COLLECTION_NEW_ADD.remove({'date':PARAMS['crawl_date']})
-    res = RAW_COLLECTION.find()
-    for r in res:
-        _id = r['_id']
-        #替换image url
-
-        save_record = {'info':r, 'update_time':datetime.now(), 'price_history.%s' % r['update_time']:r['price']}
-        #print save_record
-        if COLLECTION.find_one({'_id':_id, 'first_release_time':{'$lt':PARAMS['crawl_date']}}) == None:
-            r['create_time'] = datetime.now()
-            save_record['create_time'] = datetime.now()
-            save_record['first_release_time'] = r['update_time']
-            try:
-                region = r['region']
-                if r['region'] == u'澳門':
-                    region = 'macau'
-                elif r['region'] == u'氹仔':
-                    region = 'taipa'
-                elif r['region'] == u'路環':
-                    region = 'coloane'
-                print '%s.%s' % (region, _id)
-                COLLECTION_NEW_ADD.update({'date':PARAMS['crawl_date']}, {"$set":{'%s.%s' % (region, _id):r, '%s.%s' % ('total', _id):r}}, upsert = True)
-            except Exception as e:
-                print e
-
-        COLLECTION.update({'_id':_id}, {'$set':save_record}, upsert = True)
-
-def stat_region_data():
-    find_param = {'region':u"氹仔"}
-    count = RAW_COLLECTION.count(find_param)
-    result = {}
-    result['taipa'] = count
-
-    find_param = {'region':u"澳門"}
-    count = RAW_COLLECTION.count(find_param)
-    result['macau'] = count
-
-    find_param = {'region':u"路環"}
-    count = RAW_COLLECTION.count(find_param)
-    result['coloane'] = count
-
-    count = RAW_COLLECTION.count()
-    result['total'] = count
-    result['date'] = PARAMS['crawl_date']
-    result['insert_time'] = datetime.now()
-
-    new_add = COLLECTION_NEW_ADD.find_one({'date':PARAMS['crawl_date']})
-    #print new_add
-    if new_add:
-        result['taipa_new'] = len(new_add.get(u'taipa', {}))
-        result['macau_new'] = len(new_add.get(u'macau', {}))
-        result['coloane_new'] = len(new_add.get(u'coloane', {}))
-        result['total_new'] = len(new_add.get('total', {}))
-    else:
-        result['taipa_new'] = 0
-        result['macau_new'] = 0
-        result['coloane_new'] = 0
-        result['total_new'] = 0
-    #print result
-    COLLECTION_RESIDENCE_NUM_BY_DAY.update({'date': PARAMS['crawl_date']}, result, upsert = True)
 '''
 跟上一次价格变化数据比
 '''
@@ -328,11 +256,11 @@ COLLECTION_RESIDENCE_PRICE_TREND_MONTH = DB[settings['MONGODB_COLLECTION_PRICE_T
 DAYS_BEFORE = 30
 
 if __name__ == '__main__':
-    #print PARAMS['crawl_date']
+    print PARAMS['crawl_date']
     process_date = datetime.strptime(PARAMS['crawl_date'], '%Y-%m-%d') - timedelta(1)
     
     process_date = process_date.strftime('%Y-%m')
-    
+    print process_date
     update_all_price_trend_by_month(process_date)
     #update_all_price_trend()
     #calc_date()
